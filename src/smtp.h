5cb54b4284ceca60574394a1026da3b5540811e1
James Walker <jamesw@frameforge3d.com>
2020-01-16 14:37:05 -0800

    "Fix" a deprecation warning.  (Adding __attribute__((deprecated)) produces lots more warnings.)

--- src/smtp.h
+++ src/smtp.h
@@ -328,7 +328,7 @@ smtp_status_code_clear(struct smtp *const smtp);
  * work correctly for clearing the SMTP_STATUS_PARAM and SMTP_STATUS_FILE
  * errors. Do not use this to clear any other error codes.
  *
- * @deprecated Use @ref smtp_status_code_clear instead.
+ * deprecated Use @ref smtp_status_code_clear instead.
  *
  * @param[in] smtp            SMTP client context.
  * @param[in] new_status_code See @ref smtp_status_code.
